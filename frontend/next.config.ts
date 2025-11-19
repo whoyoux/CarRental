c48de1a5b4c6057cb11cee36615a3122b0ef193d
whoyoux <48690153+whoyoux@users.noreply.github.com>
2025-11-19 22:39:55 +0100

    add docker to front

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  output: "standalone",
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
 };
 
 export default nextConfig;
